c6b18564457327a90c027a5271b7df1d520cecea
rivalzka <suatdogan21@gmail.com>
2021-01-05 09:48:14 +0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,26 @@
+var myScroll;
+
+function loaded() {
+    myScroll = new IScroll('#scrolling-menu-wrapper', {
+        scrollX: true,
+        scrollY: false,
+        tap: true,
+        scrollbars: true,
+        interactiveScrollbars: true,
+        mouseWheel: true,
+        keyBindings: true
+    });
+
+    $('.level').on('tap', function(e) {
+        console.info('data', $(e.currentTarget).attr('data'));
+
+        //alert ('load level: ' + $(e.currentTarget).attr('data'));
+    });
+    $('.iScrollIndicator').css('display', 'none');
+
+    //  myScroll.scrollToElement(document.querySelector('#scrolling-menu-wrapper li:nth-child(1)'), null, null, true)
+}
+
+document.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+}, false);
