50085fed58e233568d26516763e1dc9b305ebfc9
rivalzka <suatdogan21@gmail.com>
2021-01-06 09:30:59 +0300

    one page test

--- src/script.js
+++ src/script.js
@@ -14,13 +14,10 @@ function loaded() {
     $('.level').on('tap', function(e) {
         console.info('data', $(e.currentTarget).attr('data'));
 
-        //alert ('load level: ' + $(e.currentTarget).attr('data'));
+        alert('load level: ' + $(e.currentTarget).attr('data'));
     });
-    $('.iScrollIndicator').css('display', 'none');
 
-    var x = "Total Width: " + screen.width + "px";
-    document.getElementById("demo").innerHTML = x;
-    //  myScroll.scrollToElement(document.querySelector('#scrolling-menu-wrapper li:nth-child(1)'), null, null, true)
+    myScroll.scrollToElement(document.querySelector('#scrolling-menu-wrapper li:nth-child(5)'), null, null, true)
 }
 
 document.addEventListener('touchmove', function(e) {
